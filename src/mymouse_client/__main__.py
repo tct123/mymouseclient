bb97dd9c85ae2015d62282c9fc7fdb311b3bb7b2
tct123 <42028373+tct123@users.noreply.github.com>
2024-07-26 09:08:31 +0200

    Initial commit

---
+++ src/mymouse_client/__main__.py
@@ -0,0 +1,4 @@
+from mymouse_client.app import main
+
+if __name__ == "__main__":
+    main().main_loop()
---
+++ src/mymouse_client/app.py
@@ -0,0 +1,63 @@
+"""
+My first application
+"""
+
+import toga
+from toga.style import Pack
+from toga.style.pack import COLUMN, ROW
+
+alphabet = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z",
+]
+
+
+def postkey(key, mode="lower"):  # mode: upper, lower
+    pass
+
+
+class MyMouseClient(toga.App):
+    def startup(self):
+        """Construct and show the Toga application.
+
+        Usually, you would add your application to a main content box.
+        We then create a main window (with a name matching the app), and
+        show the main window.
+        """
+        main_box = toga.Box(
+            children=[
+                toga.Button(text=i, on_press=lambda: postkey(key=i)) for i in alphabet
+            ]
+        )
+
+        self.main_window = toga.MainWindow(title=self.formal_name)
+        self.main_window.content = main_box
+        self.main_window.show()
+
+
+def main():
+    return MyMouseClient()
